3a2e197c057d063004a9ca6eddbd1fe95f679958
neekrasov <neekrasov@gmail.com>
2022-10-22 13:11:55 +0300

    Composite

---
+++ Composite/neural_networks.py
@@ -0,0 +1,53 @@
+import abc
+from typing import Iterable
+
+
+class Connectable(Iterable, abc.ABC):
+    def connect_to(self, other):
+        if self is other:
+            return
+        for n in self:
+            for o in other:
+                n.outputs.append(o)
+                o.inputs.append(n)
+
+
+class Neuron(Connectable):
+    def __init__(self, name):
+        self.name = name
+        self.inputs = []
+        self.outputs = []
+
+    def __iter__(self):
+        yield self
+
+    def __str__(self) -> str:
+        return f"{self.name}: {len(self.inputs)} inputs, {len(self.outputs)} outputs"
+
+
+class NeuronLayer(list, Connectable):
+    def __init__(self, name, count):
+        super().__init__()
+        self.name = name
+        for x in range(count):
+            self.append(Neuron(f"{name}-{x}"))
+
+    def __str__(self) -> str:
+        return f"{self.name} with {len(self)} neurons"
+
+
+if __name__ == "__main__":
+    neuron1 = Neuron("n1")
+    neuron2 = Neuron("n2")
+    layer1 = NeuronLayer("L1", 3)
+    layer2 = NeuronLayer("L2", 4)
+
+    neuron1.connect_to(neuron2)
+    neuron1.connect_to(layer1)
+    layer1.connect_to(neuron2)
+    layer1.connect_to(layer2)
+
+    print(neuron1)
+    print(neuron2)
+    print(layer1)
+    print(layer2)
